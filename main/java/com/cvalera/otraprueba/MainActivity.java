b5c74ef61205f5d7d233578c62c22e87481d8be4
cvalera8 <32440588+cvalera8@users.noreply.github.com>
2017-10-01 18:16:09 -0300

    Add files via upload

---
+++ main/java/com/cvalera/otraprueba/MainActivity.java
@@ -0,0 +1,21 @@
+package com.cvalera.otraprueba;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView texto;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        texto = (TextView) findViewById(R.id.textView);
+        texto.setText("NUEVO TEXTO");
+    }
+}
